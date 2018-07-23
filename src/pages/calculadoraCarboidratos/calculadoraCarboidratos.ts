a079007a2e8458f5bc1f399b410eef7601ecd57c
Adbys José Vascocnelos de Andrade <adbys.andrade@ccc.ufcg.edu.br>
2018-07-23 17:47:07 -0300

    exibindo toast

--- src/pages/calculadoraCarboidratos/calculadoraCarboidratos.ts
+++ src/pages/calculadoraCarboidratos/calculadoraCarboidratos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { Alimento } from '../../model/alimento';
+import { ToastController } from 'ionic-angular'
 
 @Component({
   selector: 'page-calculadora-carboidratos',
@@ -10,10 +11,10 @@ import { Alimento } from '../../model/alimento';
 export class CalculadoraCarboidratosPage {
 
 	url: string = "https://glicoradar.firebaseio.com/TabelaCarboidratos.json";
-	tabelaCarboidratos: Array<{nome: string, kcal: number, medidaCaseira: string, peso: number}>
+	tabelaCarboidratos: Array<{nome: string, cho: number, kcal: number, medidaCaseira: string, peso: number}>
 	alimentos: any[] = [];
 
-  constructor(public httpClient: HttpClient) {
+  constructor(public httpClient: HttpClient,public toastCtrl: ToastController,) {
   	this.alimentos = [];
   	this.obterTabelaDeCarboidratos();
 
@@ -28,10 +29,36 @@ export class CalculadoraCarboidratosPage {
    }
 
    adicionarAlimento() {
-   		var alimento = new Alimento();
+   		var alimento = {};
 
    		this.alimentos.push(alimento);
+   }
 
-   		console.log(this.alimentos)
+   private abrirToast(text) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 2000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
+
+   calcularCarboidratos() {
+   	var carb = 0;
+
+   	for (let alimento of this.alimentos) {
+   		if (alimento == null) {
+   			continue;
+   		} else {
+   			for (let element of this.tabelaCarboidratos) {
+   				if (element.nome === alimento.nome) {
+   					carb += element.cho * (alimento.quantidade/element.medidaCaseira);
+   				}
+   			}
+   		}
+   	}
+
+   	this.abrirToast("Você estará consumindo " + carb + " gramas de carboidratos");
+
    }
 }
